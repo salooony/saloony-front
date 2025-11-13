2621d21bce29653a58a4d2d1914ccaa561ee051b
MohamedEllouze <mohamed.ellouze21@gmail.com>
2025-11-13 16:36:21 +0100

    fix: auth layout type config

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,16 @@
 // project imports
+import { ReactElement } from 'react';
+import { Metadata } from 'next';
 import GuestGuard from 'utils/route-guard/GuestGuard';
 
-// types
-import { GuardProps } from 'types/auth';
+export const metadata: Metadata = {
+  title: 'Authentication',
+  description: 'Sign in or sign up to continue'
+};
+interface LayoutProps {
+  children: ReactElement | null;
+}
 
-// ==============================|| DASHBOARD LAYOUT ||============================== //
-
-export default function Layout({ children }: GuardProps) {
+export default function Layout({ children }: LayoutProps) {
   return <GuestGuard>{children}</GuestGuard>;
 }
